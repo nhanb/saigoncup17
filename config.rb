1dbdc9773e106221ed0d4ab197059252ce9f5121
nhanb <nhan@nerdyweekly.com>
2017-03-22 21:38:52 +0700

    minify css, gzip

--- config.rb
+++ config.rb
@@ -38,8 +38,9 @@ end
 
 # Build-specific configuration
 configure :build do
+  activate :gzip
   # Minify CSS on build
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
   # activate :minify_javascript
